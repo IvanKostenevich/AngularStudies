f3861d733f298aa5470f812d0c29cf7087a01219
IvanKostenevich <ivan.kostenevich91@gmail.com>
2016-08-26 14:20:09 +0300

    Reworked with the help of UI Router

--- app.module.js
+++ app.module.js
@@ -1,8 +1,22 @@
 'use strict';
-angular.module('plantApp', [
-    'partList',
-    'productionQueue'
+var plantApp = angular.module('plantApp', [
+    'ui.router'
+])
+plantApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('products', {
+            url: '/products',
+            templateUrl: 'app/spare-parts-list.template.html',
+            controller: 'sparePartsCtrl'
 
-    
-]);
+        })
+        .state('factory', {
+            url: '/factory',
+            templateUrl: 'app/production-queue.template.html',
+            controller: 'productionQueueCtrl'
+
+        })
+
+})
 
--- app/partList.component.js
+++ app/partList.component.js
@@ -1,19 +1,15 @@
-'use strict';
-angular.module('partList').component('partList', {
-    templateUrl: 'app/spare-parts-list.template.html',
-    controller: function sparePartsCtrl() {
-        this.spareImg = [
-            {
-                name: 'Bicycle part',
-                image: 'images/image1.jpg'
-            }, {
-                name: 'Motorcycle part',
-                image: 'images/image2.jpg'
-            }, {
-                name: 'Scooter part',
-                image: 'images/image3.jpg'
-            }
-        ];
+plantApp.controller('sparePartsCtrl',function($scope) {
+    $scope.spareImg = [
+        {
+            name: 'Bicycle part',
+            image: 'images/image1.jpg'
+        }, {
+            name: 'Motorcycle part',
+            image: 'images/image2.jpg'
+        }, {
+            name: 'Scooter part',
+            image: 'images/image3.jpg'
+        }
+    ];
 
-    }
-})
+});
